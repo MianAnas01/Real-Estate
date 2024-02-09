10b2de5d429acdca618054b2e5ab61ad0c4c81bc
M Anas <89442277+MianAnas01@users.noreply.github.com>
2024-02-09 18:45:55 +0500

    dummy

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from './routes/user.route.js';
+ 
 dotenv.config();
  mongoose.connect(process.env.MONGO_STRING)
   .then(() => {
@@ -13,3 +15,22 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+// const startServer = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_STRING);
+//     console.log('Connected to MongoDB!');
+//     const app = express();  
+//     app.listen(3000, () => {
+//       console.log("Server is running on port 3000");
+//     });
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//   }
+// };
+// export default startServer();
+
+app.use("/api/user ", userRouter);
