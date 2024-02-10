5e4b487a8cca8f9eef2c2eaa1e8e0a83797d76f0
M Anas <89442277+MianAnas01@users.noreply.github.com>
2024-02-10 12:59:41 +0500

    signup api created

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
 import express from "express";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 const app = express();
+
+app.use(express.json());
 
 (async () => {
   try {
@@ -22,3 +26,4 @@ const app = express();
 })();
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
