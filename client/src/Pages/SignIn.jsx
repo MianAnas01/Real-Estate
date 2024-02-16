12b2baf565481e050496c3f49753e326063b6955
M Anas <89442277+MianAnas01@users.noreply.github.com>
2024-02-16 16:05:08 +0500

    added rwedux toolkit

--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,20 +1,26 @@
 
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../Redux/user/userSlice';
 
 function SignIn() {
-  const [formData, setFormData] =useState({});
-const [error, setError] = useState('null');
-const [loading, setLoading] = useState(false);
+
+const [formData, setFormData] =useState({});
+const { loading, error } = useSelector((state) => state.user);
 const navigate = useNavigate();
+const dispatch = useDispatch();
+
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value
     });
   };
+
 const handleSubmit = async (e) => {
 e.preventDefault(); 
+
 try {
-  setLoading(true);
+dispatch(signInStart());
 const res = await fetch('/api/auth/signin',
 {
   method: 'POST',
@@ -26,17 +32,15 @@ const res = await fetch('/api/auth/signin',
 const data = await res.json();
 console.log(data);
 if(data.success == false) {
-  setLoading(false);
-  setError(data.message);
+ dispatch(signInFailure(data.message));
   return;
 };
-setLoading(false);
-setError(null);
+dispatch(signInSuccess(data)); 
 navigate('/');
 }
 catch (error) {   
-  setLoading(false);
-  setError(error.message);
+  dispatch(signInFailure(error.message));
+  
 }
 };
 console.log(formData);
@@ -56,8 +60,12 @@ className='border p-3  rounded-lg' id='email'
 className='border p-3  rounded-lg' id='password' 
 onChange={handleChange}/>
 
-<button className='bg-slate-700 text-white p-3 rounded-lg 
-uppercase hover:opacity-95 disabled:opacity-80'> Sign in</button>
+<button
+         disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign In'}
+        </button>
  </form>
 
  <div className="flex gap-2 mt-5">
