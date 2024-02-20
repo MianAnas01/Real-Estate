cc758dff4b159659415f11e0e04eb4fdf06667a3
M Anas <manas4808294@gmail.com>
2024-02-20 15:34:08 +0500

    all set

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,31 +1,29 @@
-import {createSlice} from "@reduxjs/toolkit";
-
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUser:  null,
-    loading: false,
-    error: null,
-}
+  currUser: null,
+  error: null,
+  loading: false,
+};
 
 const userSlice = createSlice({
-name: "user",
-initialState,
-reducers: { 
-    signInStart: (state) => {   
-        state.loading = true;
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
     },
     signInSuccess: (state, action) => {
-        state.loading = false;
-        state.currentUser = action.payload;
-        state.error = null;
+      state.currUser = action.payload;
+      state.loading = false;
+      state.error = null;
     },
     signInFailure: (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
+      state.error = action.payload;
+      state.loading = false;
     },
-},
+  },
 });
 
-export const {signInStart, signInSuccess, signInFailure} = userSlice.actions;
-
-export default userSlice;
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export default userSlice.reducer;
