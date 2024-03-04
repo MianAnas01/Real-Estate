041bd11de134ecff288ae86e6e79c8d209b1d321
M Anas <manas4808294@gmail.com>
2024-03-04 16:22:43 +0500

    completed delete user listing

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -8,5 +8,23 @@ return res.status(201).json(listing);
 } catch (error) {
     next(error);
 }
+};
 
+export const deleteListing = async (req, res, next) => {
+const listing= await Listing.findById(req.params.id);
+
+if (!listing) {
+return next(errorHandler(404, 'listing not found')); 
+}
+
+if(req.user.id !== listing.userRef){
+    return next(errorHandler(401, 'you can only delete your own listings'));
+}
+try {
+await Listing.findByIdAndDelete(req.params.id);
+res.status(200).json('listing deleted');
+}
+catch (error) {
+next(error);
+}
 };
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -145,24 +145,24 @@ const handleSignOut = async () => {
     }
   };
 
-  // const handleListingDelete = async (listingId ) => {
-  //   try {
-  //     const res = await fetch(`/api/listing/delete/${listingId}`, {
-  //       method: "DELETE",
-  //     });
-  //     const data = await res.json();
-  //     if (data.success === false) {
-  //       console.log(data.message);
-  //       return;
-  //     }
+  const handleListingDelete = async (listingId ) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
 
-  //     setUserListings((prev) =>
-  //       prev.filter((listing) => listing._id !== listingId)
-  //     );
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -266,26 +266,26 @@ const handleSignOut = async () => {
               className="border rounded-lg p-3 flex gap-4 justify-between items-center"
               key={listing._id}
             >
-              <Link to={`/listing/${listing._id}`}>
+            
+             <Link to={`/listing/${listing._id}`}>
                 <img
                   src={listing.imageUrls[0]}
                   alt="listing cover"
                   className="h-16 w-16 object-contain"
                 />
               </Link>
+              
               <Link
                 className="text-slate-700 font-semibold flex-1 hover:underline truncate"
                 to={`/listing/${listing._id}`}
               >
                 <p>{listing.name}</p>
               </Link>
+             
               <div className="flex flex-col items-center">
                 <button
                   onClick={() => handleListingDelete(listing._id)}
-                  className="text-red-700 uppercase"
-                >
-                  Delete
-                </button>
+                  className="text-red-700 uppercase">  Delete </button>
                 <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
